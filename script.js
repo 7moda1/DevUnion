406ad7b2b5cf407468f3f96c9f8461056b5dac3e
7moda1 <Bomb.bomb2772@gmail.com>
2025-06-09 03:31:49 +0300

    remove broken submodule and add folder as normal

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ document.addEventListener('DOMContentLoaded', () => {
         localStorage.setItem('theme', selectedTheme);
     });
 
+
+
+
+    
     // === إعداد الخلفية ثلاثية الأبعاد ===
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
